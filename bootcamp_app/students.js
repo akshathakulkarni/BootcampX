c16b6a13ec9d66144c95fa40bd7235a155d36b38
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-10-11 15:47:53 -0400

    Update files to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,16 +8,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+//Data from user
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
 //Query to get student data
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
 //pool.query is a function that accepts an SQL query as a JavaScript string
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-//function returns a promise with response when query is successful
+pool.query(queryString, values)
+  //function returns a promise with response when query is successful
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
